8fa5ee427573cb7f932626949a216b6758b82b0d
Jason Kearney <jkearney345@gmail.com>
2022-07-09 16:21:23 -0400

    create operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case add:
+            return add(num1, num2);
+            break;
+        case subtract:
+            return subtract(num1, num2);
+            break;
+        case multiply:
+            return multiply(num1, num2);
+            break;
+        case divide:
+            return divide(num1, num2);
+            break;
+    }
+}
